3f4ce6e4ffde3ecc5f8e84fce2461e18be870a9f
Brelok <code2pradelok@gmail.com>
2020-02-11 22:09:47 +0100

    logg sucessful but not working on every subpage

--- src/brelok/workshops/MyBenefit.java
+++ src/brelok/workshops/MyBenefit.java
@@ -6,23 +6,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import javax.json.*;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+
+
 
 public class MyBenefit {
     final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15";
     final static String LOGIN_FORM_URL = "https://system.mybenefit.pl/mybenefit/login.html";
     final static String LOGIN_ACTION_URL = "https://system.mybenefit.pl/mybenefit/login/by_email";
+    final static String URL = "https://system.mybenefit.pl/mybenefit/benefit-login-dispatcher.html";
+    final static String URLQUERY = "https://system.mybenefit.pl/mybenefit/global-search.html?query=";
     final static String USERNAME = "xxx";
     final static String PASSWORD = "xxx";
 
     public static void main(String[] args) {
-
-
-        List<String> headers = new ArrayList<>();
-        Path path = Paths.get("/Users/brelok/workspace/myBenefit/headers.txt");
 
         try {
             Connection.Response loginForm = Jsoup.connect(LOGIN_FORM_URL)
@@ -30,29 +35,44 @@ public class MyBenefit {
                     .userAgent(USER_AGENT)
                     .execute();
 
-            HashMap<String, String> cookies = new HashMap<>(loginForm.cookies());
+            JsonBuilderFactory jsonBuilderFactory = Json.createBuilderFactory(Collections.EMPTY_MAP);
+            JsonObject jsonObject = jsonBuilderFactory.createObjectBuilder()
+                    .add("password", PASSWORD)
+                    .add("captcha", "")
+                    .add("lang", "pl")
+                    .add("email", USERNAME).build();
 
-            HashMap<String, String> formData = new HashMap<>();
-            formData.put("captcha", "");
-            formData.put("email", USERNAME);
-            formData.put("lang", "pl");
-            formData.put("password", PASSWORD);
+
+            HashMap<String, String> cookies = new HashMap<>(loginForm.cookies());
 
             Connection.Response homePage = Jsoup.connect(LOGIN_ACTION_URL)
                     .cookies(cookies)
                     .ignoreContentType(true)
-                    .data(formData)
+                    .requestBody(jsonObject.toString())
                     .method(Connection.Method.POST)
                     .userAgent(USER_AGENT)
                     .execute();
 
-            System.out.println(homePage.statusCode());
-            System.out.println(homePage.parse());
+            //here I get cookie after logg
+            String sessionID = homePage.cookie("BENEFIT_SESSIONID");
+
+            //this connection is working
+            Connection.Response afterLogg = Jsoup.connect(URL)
+                    .cookie("BENEFIT_SESSIONID", sessionID)
+                    .execute();
+
+            Document document = afterLogg.parse();
+
+            //this connection is NOT workingg good, I am not logged on website
+            Connection.Response test = Jsoup.connect(URLQUERY)
+                    .cookie("BENEFIT_SESSIONID", sessionID)
+                    .execute();
+
+            System.out.println(test.parse());
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
